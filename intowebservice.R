459361170e553378539f001c2e6a71467ccf2233
ningchen <ningchen@ningchende-MacBook-Pro.local>
2019-09-28 14:04:45 +0800

    updated intowebsercice.R library and typo

--- intowebservice.R
+++ intowebservice.R
@@ -1,5 +1,7 @@
+# library(EBImage)
+# library(httr)
 
-a= readImage("../output/7_2018-12-14_081753.png")
+import_color.image= readImage("~/Downloads/shiny_mnist/output/2_2018-12-14_081819.png")
 import_gray.image <- channel(import_color.image,"gray")
 # 使用EBImage channel function 轉成灰階
 resize_gray.image <- resize(import_gray.image, 28, 28) 
@@ -7,7 +9,7 @@ resize_gray.image <- resize(import_gray.image, 28, 28)
 trans_gray.image <- array_reshape(1-resize_gray.image,c(1,28*28))
 # 將28*28的array轉成list
 res=POST(url="http://5f8c233f-8693-4046-9117-98739a2dcf18.southeastasia.azurecontainer.io/score",
-         body=list(data = b),
+         body=list(data = trans_gray.image),
          encode="json")
 DisplayRes=httr::content(res)
 
